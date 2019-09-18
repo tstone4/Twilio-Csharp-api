d3a2d0f6c9632e674acca7162e8836011b8eaa18
Thomas Stone <tstone@epitec.com>
2019-09-18 16:42:33 -0400

    Has the first controller.  Always starts spotify on my computer

---
+++ toms idiot server/Controllers/SmsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace toms_idiot_server.Controllers
+{
+    public class SmsController : Controller
+    {
+        [HttpGet]
+        [Route("sms")]
+        public void Index()
+        {
+            var psi = new ProcessStartInfo() { FileName = @"C:\Users\tstone\AppData\Roaming\Spotify\Spotify.exe", UseShellExecute = true };
+            Process.Start(psi);
+        }
+    }
+}
